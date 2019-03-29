0c7339d3ebb00865e10ada355b7c79014c1f1796
Miller <sbrn290@pfslap859648.home>
2019-03-29 10:39:53 +0000

    Latest bits

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -26,6 +26,10 @@ class Firebase {
 
   doSignOut = () => this.auth.signOut();
 
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password =>
+    this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase
